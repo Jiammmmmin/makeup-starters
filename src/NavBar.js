235c45fc16537fb82bafe61a8a0958a28af3a2ca
Jiammmmmin <97135819+Jiammmmmin@users.noreply.github.com>
2023-03-18 21:45:54 -0700

    s
    q

---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import './index.css'
+export function NavBar(props){
+    return(
+        <header>
+            <h1>Makeup Starter</h1>
+            <nav>
+                <div class="navbar-links">
+                    {/* <Link to='/profile'> profile </Link> */}
+                </div>
+            </nav>
+            <h2>Test your skin Tempterature!</h2>
+        </header>
+    );
+}
